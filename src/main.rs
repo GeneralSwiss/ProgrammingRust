ef50960892a90dd2d1e7be9dc517490836a43d6a
Nick <nicolas.farrier@gmail.com>
2022-04-27 20:51:39 -0700

    Update gcd to use command line for input and add dependencies for next tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod gcd;
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut nums: Vec<u64> = Vec::new();
+    for arg in env::args().skip(1) {
+        nums.push(u64::from_str(&arg).expect("Unable to parse argument into a number."));
+    }
+
+    let mut answer = nums[0];
+    for num in &nums[1..] {
+        answer = gcd::gcd(answer, *num);
+    }
+
+    println!("The GCD between {:?} is {}", nums, answer);
 }
